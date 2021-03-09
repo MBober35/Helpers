38e5958384cfc9493fd769ee85b13b7b34848d8d
Programmer <peshkov@mbober.ru>
2021-03-09 16:19:09 +0300

    add simple prop

--- src/resources/js/components/ErrorRenderComponent.vue
+++ src/resources/js/components/ErrorRenderComponent.vue
@@ -1,56 +1,63 @@
 <template>
-    <div class="alert alert-danger" role="alert" v-if="Object.keys(errors).length">
-        <template v-for="field in errors">
-            <template v-for="error in field">
-                <div class="alert-message">{{ error }}</div>
-                <br>
-            </template>
-        </template>
-    </div>
+  <div class="alert alert-danger" role="alert" v-if="Object.keys(errors).length">
+    <template v-for="field in errors">
+      <template v-for="error in field">
+        <div :class="! simple ? 'alert-message' : ''">{{ error }}</div>
+        <br v-if="! simple">
+      </template>
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            errors: {}
-        }
+  props: {
+    simple: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      errors: {}
+    }
+  },
+
+  methods: {
+    resetErrors() {
+      this.errors = {};
     },
 
-    methods: {
-        resetErrors() {
-            this.errors = {};
-        },
+    // Обработать ошибки валидации.
+    parseErrors(result) {
+      if (result.hasOwnProperty("errors")) {
+        this.errors = result.errors;
+      } else {
+        this.fireError(result.message);
+      }
+    },
 
-        // Обработать ошибки валидации.
-        parseErrors(result) {
-            if (result.hasOwnProperty("errors")) {
-                this.errors = result.errors;
-            } else {
-                this.fireError(result.message);
-            }
-        },
+    // Вызвать подтверждение.
+    fireSuccess(message) {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: message,
+        timer: 1500
+      })
+    },
 
-        // Вызвать подтверждение.
-        fireSuccess(message) {
-            Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: message,
-                timer: 1500
-            })
-        },
-
-        // Вызвать ошибку.
-        fireError(message) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Упс...',
-                text: 'Что-то пошло не так!',
-                footer: message,
-            })
-        }
+    // Вызвать ошибку.
+    fireError(message) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Упс...',
+        text: 'Что-то пошло не так!',
+        footer: message,
+      })
     }
+  }
 }
 </script>
 
